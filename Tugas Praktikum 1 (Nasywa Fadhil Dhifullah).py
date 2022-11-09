4d1b052b11a9aa141d8c01b45290dafc967d5e00
Nasywa Fadhil Dhifullah <113097546+Dhilz12@users.noreply.github.com>
2022-11-09 15:09:49 +0700

    Add files via upload
    Membuat biodata diri sederhana

---
+++ Tugas Praktikum 1 (Nasywa Fadhil Dhifullah).py
@@ -0,0 +1,16 @@
+#tugas praktikum 1
+#program biodata sederhana
+
+Pesan = ("Halo semuanya")
+input (Pesan)               
+
+Nama                 =  "Nasywa Fadhil Dhfiullah,"
+nim                  =  "22241010102038,"
+Tempat_Tanggal_Lahir =  "Karawang, 12 September 2004,"
+Umur                 =  "17 Tahun,"
+Jenis_Kelamin        =  "Laki-laki,"
+Jurusan              =  "Teknologi Informasi,"
+Alamat               =  "Jln. Jawa 7, Des. Sumbersari, Kec. Sumbersari"
+
+print (" Perkenalkan namaku", Nama,"NIM ku", nim, "aku lahir di", Tempat_Tanggal_Lahir, "sekarang umurku", Umur,
+       "aku", Jenis_Kelamin, "aku dari jurusan", Jurusan, "dan alamatku di", Alamat) 
